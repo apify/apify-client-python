c8c8f826515d540fda767959e57f98f78d182386
Jan Curn <jan.curn@gmail.com>
2019-01-08 13:57:44 +0100

    Initial commit

---
+++ apify_client/__init__.py
@@ -0,0 +1,1 @@
+name = "apify_client"
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="apify_client",
+    version="0.0.1",
+    author="Apify Technologies s.r.o.",
+    author_email="support@apify.com",
+    description="Work in progress: Apify API client for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+

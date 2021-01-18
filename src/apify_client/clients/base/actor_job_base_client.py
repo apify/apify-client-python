2e403e0e6120b34c820bec8e81c7bd4bbfcaee37
Matej Hamas <matej.hamas@gmail.com>
2021-01-18 11:28:13 +0100

    Address comments:

--- src/apify_client/clients/base/actor_job_base_client.py
+++ src/apify_client/clients/base/actor_job_base_client.py
@@ -1,40 +1,60 @@
 import math
+import time
 from datetime import datetime
 from typing import Dict, Optional
 
 from ..._consts import TERMINAL_ACTOR_JOB_STATUSES, ActorJobStatus
-from ..._utils import _parse_date_fields, _pluck_data
+from ..._errors import ApifyApiError
+from ..._utils import _catch_not_found_or_throw, _parse_date_fields, _pluck_data
 from .resource_client import ResourceClient
+
+DEFAULT_WAIT_FOR_FINISH = 999999
 
 
 class ActorJobBaseClient(ResourceClient):
     """Base sub-client class for actor runs and actor builds."""
 
-    def _wait_for_finish(self, wait_secs: Optional[int] = None) -> Dict:
+    def _wait_for_finish(self, wait_secs: Optional[int] = None) -> Optional[Dict]:
         started_at = datetime.now()
         should_repeat = True
         job: Optional[Dict] = None
         seconds_elapsed = 0
 
         while(should_repeat):
-            wait_for_finish = 9999
+            wait_for_finish = DEFAULT_WAIT_FOR_FINISH
             if wait_secs is not None:
                 wait_for_finish = wait_secs - seconds_elapsed
 
-            response = self.http_client.call(
-                url=self.url,
-                method='GET',
-                params=self._params(wait_for_finish=wait_for_finish),
-            )
-            job = _parse_date_fields(_pluck_data(response.json()))
+            try:
+                response = self.http_client.call(
+                    url=self._url(),
+                    method='GET',
+                    params=self._params(waitForFinish=wait_for_finish),
+                )
+                job = _parse_date_fields(_pluck_data(response.json()))
 
-            seconds_elapsed = math.floor(((datetime.now() - started_at).total_seconds()))
-            if (
-                ActorJobStatus(job['status']) in TERMINAL_ACTOR_JOB_STATUSES or (wait_secs is not None and seconds_elapsed >= wait_secs)
-            ):
-                should_repeat = False
+                seconds_elapsed = math.floor(((datetime.now() - started_at).total_seconds()))
+                if (
+                    ActorJobStatus(job['status']) in TERMINAL_ACTOR_JOB_STATUSES or (wait_secs is not None and seconds_elapsed >= wait_secs)
+                ):
+                    should_repeat = False
 
-        if job is None:
-            raise ValueError(f"Waiting for actor job with ID {self.resource_id} failed.")
+                if not should_repeat:
+                    # Early return here so that we avoid the sleep below if not needed
+                    return job
+
+                # It might take some time for database replicas to get up-to-date so sleep a bit before retrying
+                time.sleep(250)
+
+            except ApifyApiError as exc:
+                _catch_not_found_or_throw(exc)
 
         return job
+
+    def _abort(self) -> Dict:
+        response = self.http_client.call(
+            url=self._url('abort'),
+            method='POST',
+            params=self._params(),
+        )
+        return _parse_date_fields(_pluck_data(response.json()))
--- src/apify_client/clients/resource_clients/build.py
+++ src/apify_client/clients/resource_clients/build.py
@@ -1,6 +1,5 @@
 from typing import Any, Dict, Optional
 
-from ..._utils import _parse_date_fields, _pluck_data
 from ..base.actor_job_base_client import ActorJobBaseClient
 
 
@@ -29,15 +28,13 @@ class BuildClient(ActorJobBaseClient):
         Returns:
             The data of the aborted actor build
         """
-        response = self.http_client.call(
-            url=self._url('abort'),
-            method='POST',
-            params=self._params(),
-        )
-        return _parse_date_fields(_pluck_data(response.json()))
+        return self._abort()
 
-    def wait_for_finish(self, *, wait_secs: Optional[int] = None) -> Dict:
+    def wait_for_finish(self, *, wait_secs: Optional[int] = None) -> Optional[Dict]:
         """Wait synchronously until the build finishes or the server times out.
+
+        Args:
+            wait_secs (Optional[int]): how long does the client wait for build to finish. None for indefinite.
 
         Returns:
             The actor build data. If the status on the object is not one of the terminal statuses (SUCEEDED, FAILED, TIMED_OUT, ABORTED)

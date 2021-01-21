4a90a04ec9831335809bc6ad7156c0fb16b17692
Matej Hamas <matej.hamas@gmail.com>
2021-01-21 16:51:14 +0100

    Address comment

--- src/apify_client/clients/base/actor_job_base_client.py
+++ src/apify_client/clients/base/actor_job_base_client.py
@@ -8,7 +8,10 @@ from ..._errors import ApifyApiError
 from ..._utils import _catch_not_found_or_throw, _parse_date_fields, _pluck_data
 from .resource_client import ResourceClient
 
-DEFAULT_WAIT_FOR_FINISH = 999999
+DEFAULT_WAIT_FOR_FINISH_SEC = 999999
+
+# After how many seconds we give up trying in case job doesn't exist
+DEFAULT_WAIT_WHEN_JOB_NOT_EXIST_SEC = 3
 
 
 class ActorJobBaseClient(ResourceClient):
@@ -21,7 +24,7 @@ class ActorJobBaseClient(ResourceClient):
         seconds_elapsed = 0
 
         while should_repeat:
-            wait_for_finish = DEFAULT_WAIT_FOR_FINISH
+            wait_for_finish = DEFAULT_WAIT_FOR_FINISH_SEC
             if wait_secs is not None:
                 wait_for_finish = wait_secs - seconds_elapsed
 
@@ -45,6 +48,11 @@ class ActorJobBaseClient(ResourceClient):
 
             except ApifyApiError as exc:
                 _catch_not_found_or_throw(exc)
+
+                # If there are still not found errors after DEFAULT_WAIT_WHEN_JOB_NOT_EXIST_SEC, we give up and return None
+                # In such case, the requested record probably really doesn't exist.
+                if (seconds_elapsed > DEFAULT_WAIT_WHEN_JOB_NOT_EXIST_SEC):
+                    return None
 
             # It might take some time for database replicas to get up-to-date so sleep a bit before retrying
             time.sleep(250)

c2efb8e2f41a73f9d0e5ce03c8bedccb5b5a9420
Matej Hamas <matej.hamas@apify.com>
2021-01-22 14:24:08 +0100

    Add subclient for querying all builds for a user
    * Add client for manipulating a single build object
    
    * Add subclient for querying all builds for a user
    
    * Address comments:
    
    * Add args description
    
    * Address comments
    
    * Address comment
    
    Co-authored-by: František Nesveda <frantisek@apify.com>

---
+++ src/apify_client/clients/resource_clients/build_collection.py
@@ -0,0 +1,28 @@
+from typing import Any, Dict, Optional
+
+from ..base.resource_collection_client import ResourceCollectionClient
+
+
+class BuildCollectionClient(ResourceCollectionClient):
+    """Sub-client for listing user builds.
+
+    Note that this client is not specific for a particular actor but queries all builds for a user based on the provided API token.
+    """
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Initialize the BuildCollectionClient."""
+        super().__init__(*args, resource_path='actor-builds', **kwargs)
+
+    def list(self, *, limit: Optional[int] = None, offset: Optional[int] = None, desc: Optional[bool] = None) -> Dict:
+        """List all builds of a user.
+
+        https://docs.apify.com/api/v2#/reference/actor-builds/build-collection/get-user-builds-list
+        Args:
+            limit: How many builds to retrieve
+            offset: What build store to include as first when retrieving the list
+            desc: Whether to sort the builds in descending order based on their modification date
+
+        Returns:
+            The retrieved builds of a user
+        """
+        return self._list(limit=limit, offset=offset, desc=desc)

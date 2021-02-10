308e4de88126dcc5c9d6695f6370ff11c122cbde
Equidem <matsochor@seznam.cz>
2021-02-10 23:54:02 +0100

    Fixing the typing issue

--- src/apify_client/clients/resource_clients/webhook_dispatch_collection.py
+++ src/apify_client/clients/resource_clients/webhook_dispatch_collection.py
@@ -8,7 +8,8 @@ class WebhookDispatchCollectionClient(ResourceCollectionClient):
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         """Initialize the WebhookDispatchCollectionClient."""
-        super().__init__(*args, **{'resource_path': 'webhook-dispatches', **kwargs})
+        resource_path = kwargs.pop('resource_path', 'webhook-dispatches')
+        super().__init__(*args, resource_path=resource_path, **kwargs)
 
     def list(self, *, limit: Optional[int] = None, offset: Optional[int] = None, desc: Optional[bool] = None) -> Dict:
         """List all webhook dispatches of a user.

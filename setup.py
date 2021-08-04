b4fa311188b4131b1cc93e401e2b2d9c45d17ad2
František Nesveda <frantisek@apify.com>
2021-08-04 12:25:48 +0200

    Update `requests` to 2.26.0

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     python_requires='>=3.7',
-    install_requires=['requests ~= 2.25.1'],
+    install_requires=['requests ~= 2.26.0'],
     extras_require={
         'dev': [
             'autopep8 ~= 1.5.7',

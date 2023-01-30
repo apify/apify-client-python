dfff39965a24ff660c203940e216dec867ff889a
Jindřich Bär <jindrichbar@gmail.com>
2023-01-30 16:18:15 +0100

    fix: get started button link

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -33,7 +33,7 @@ function Hero() {
                     <div className="row">
                         <div className="col">
                             <div className={styles.heroButtons}>
-                                <Link to="docs/docs" className={styles.getStarted}>Get Started</Link>
+                                <Link to="docs/intro" className={styles.getStarted}>Get Started</Link>
                                 <iframe src="https://ghbtns.com/github-btn.html?user=apify&repo=apify-client-python&type=star&count=true&size=large" width="170" height="30" title="GitHub"></iframe>
                             </div>
                         </div>

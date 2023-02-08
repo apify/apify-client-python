d2610003cc11040a517daf7df95766859010727f
Martin Adámek <banan23@gmail.com>
2023-02-08 13:00:50 +0100

    docs: add external icon to GH link

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -59,7 +59,7 @@ module.exports = {
                             activeBaseRegex: 'changelog',
                         },
                         {
-                            to: 'https://github.com/apify/apify-client-python',
+                            href: 'https://github.com/apify/apify-client-python',
                             label: 'GitHub',
                             position: 'left',
                         },

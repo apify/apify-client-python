9d478c81f967602d6c273ee36f2e786d83064031
Vlada Dusek <v.dusek96@gmail.com>
2023-07-28 11:23:26 +0200

    Do not import all consts

--- src/apify_client/consts.py
+++ src/apify_client/consts.py
@@ -1,40 +1,14 @@
 import warnings
 from typing import Any
 
-from apify_shared.consts import BOOL_ENV_VARS as BOOL_ENV_VARS_
-from apify_shared.consts import BOOL_ENV_VARS_TYPE as BOOL_ENV_VARS_TYPE_
-from apify_shared.consts import DATETIME_ENV_VARS as DATETIME_ENV_VARS_
-from apify_shared.consts import DATETIME_ENV_VARS_TYPE as DATETIME_ENV_VARS_TYPE_
-from apify_shared.consts import FLOAT_ENV_VARS as FLOAT_ENV_VARS_
-from apify_shared.consts import FLOAT_ENV_VARS_TYPE as FLOAT_ENV_VARS_TYPE_
-from apify_shared.consts import INTEGER_ENV_VARS as INTEGER_ENV_VARS_
-from apify_shared.consts import INTEGER_ENV_VARS_TYPE as INTEGER_ENV_VARS_TYPE_
-from apify_shared.consts import STRING_ENV_VARS as STRING_ENV_VARS_
-from apify_shared.consts import STRING_ENV_VARS_TYPE as STRING_ENV_VARS_TYPE_
-from apify_shared.consts import ActorEventTypes as ActorEventTypes_
-from apify_shared.consts import ActorExitCodes as ActorExitCodes_
 from apify_shared.consts import ActorJobStatus as ActorJobStatus_
 from apify_shared.consts import ActorSourceType as ActorSourceType_
-from apify_shared.consts import ApifyEnvVars as ApifyEnvVars_
 from apify_shared.consts import MetaOrigin as MetaOrigin_
 from apify_shared.consts import WebhookEventType as WebhookEventType_
 
 DEPRECATED_NAMES = [
-    'BOOL_ENV_VARS',
-    'BOOL_ENV_VARS_TYPE',
-    'DATETIME_ENV_VARS',
-    'DATETIME_ENV_VARS_TYPE',
-    'FLOAT_ENV_VARS',
-    'FLOAT_ENV_VARS_TYPE',
-    'INTEGER_ENV_VARS',
-    'INTEGER_ENV_VARS_TYPE',
-    'STRING_ENV_VARS',
-    'STRING_ENV_VARS_TYPE',
-    'ActorEventTypes',
-    'ActorExitCodes',
     'ActorJobStatus',
     'ActorSourceType',
-    'ApifyEnvVars',
     'MetaOrigin',
     'WebhookEventType',
 ]

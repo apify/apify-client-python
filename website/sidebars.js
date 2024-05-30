457a7793c6ee1fbd235a6f26f67e4c6739596939
Jan Bárta <45016873+jbartadev@users.noreply.github.com>
2024-05-30 13:44:56 +0200

    docs: Added new getting started page (#232)
    I tried to do the same thing as we did for the JS client
    [here](https://github.com/apify/apify-client-js/pull/542) but for
    Python.
    
    The goal is to improve onboarding (adaptation) for using our API (in
    Python in this case)
    
    <img width="1768" alt="Snímek obrazovky 2024-05-29 v 17 17 17"
    src="https://github.com/apify/apify-client-python/assets/45016873/bbb4a7ae-5542-43a5-9ae9-182715cce88c">
    
    ---------
    
    Co-authored-by: Vlada Dusek <v.dusek96@gmail.com>

--- website/sidebars.js
+++ website/sidebars.js
@@ -2,23 +2,11 @@ module.exports = {
     sidebar: [
         {
             type: 'doc',
-            id: 'quick-start',
-        },
-        {
-            type: 'doc',
-            id: 'features',
-        },
-        {
-            type: 'doc',
-            id: 'usage-concepts',
+            id: 'index',
         },
         {
             type: 'doc',
             id: 'examples',
-        },
-        {
-            type: 'doc',
-            id: 'changelog',
         },
     ],
 };

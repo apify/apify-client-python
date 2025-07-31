d9e16f5199ad8670d7d997c4302d1a6189ce9411
HonzaTuron <jan.turon@apify.com>
2025-07-31 16:47:23 +0200

    chore: Enable descriptions

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -109,7 +109,6 @@ module.exports = {
         [
             '@signalwire/docusaurus-plugin-llms-txt',
             {
-                enableDescriptions: false,
                 content: {
                     includeVersionedDocs: false,
                     enableLlmsFullTxt: true,

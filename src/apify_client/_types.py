3e5c56213299128c95a9b745041a1c51f7ff1125
Vlada Dusek <v.dusek96@gmail.com>
2025-12-19 11:24:09 +0100

    fix

--- src/apify_client/_types.py
+++ src/apify_client/_types.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 from dataclasses import dataclass, field
 from typing import Any, Generic, TypeVar
 
-JSONSerializable = str | int | float | bool | None | dict[str, Any] | list[Any]
+JsonSerializable = str | int | float | bool | None | dict[str, Any] | list[Any]
 """Type for representing json-serializable values. It's close enough to the real thing supported by json.parse.
 It was suggested in a discussion with (and approved by) Guido van Rossum, so I'd consider it correct enough.
 """

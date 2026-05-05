1bc7a85965c9dcad6212242a02723a14ae7f446a
Jindřich Bär <jindrichbar@gmail.com>
2026-05-05 17:06:04 +0200

    docs: ApiLink generates correct URL for versioned docs

--- website/src/components/ApiLink.jsx
+++ website/src/components/ApiLink.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import Link from '@docusaurus/Link';
+import { useDocsVersion } from '@docusaurus/plugin-content-docs/client';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 const ApiLink = ({ to, children }) => {
+    const version = useDocsVersion();
+    const { siteConfig } = useDocusaurusContext();
+
+    if (siteConfig.presets[0][1].docs.disableVersioning || version.isLast) {
+        return (
+            <Link to={`/api/${to}`}>{children}</Link>
+        );
+    }
+
     return (
-        <Link to={`/reference/${to}`}>{children}</Link>
+        <Link to={`/api/${version.version === 'current' ? 'next' : version.version}/${to}`}>{children}</Link>
     );
 };
 
